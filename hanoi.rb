64ee53c65be3505689c8b022955085f57e09d96e
Isaac Betesh <iybetesh@gmail.com>
2013-01-27 10:38:33 -0500

    initial class structure

---
+++ hanoi.rb
@@ -0,0 +1,19 @@
+class Hanoi
+  attr_reader :left, :middle, :right
+
+  def initialize(ring_count)
+  end
+
+  def play!
+  end
+
+  private
+    def validate_ring_count(ring_count)
+    end
+
+    def move
+    end
+
+    def are_we_finished?
+    end
+end
